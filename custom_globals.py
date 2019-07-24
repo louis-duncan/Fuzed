d3e76b98eba4abd1621a1eab2c35e4bdaf9931c9
Louis <louisthurman@gmail.com>
2019-07-24 18:58:13 +0100

    Changed config path.

--- custom_globals.py
+++ custom_globals.py
@@ -1,5 +1,8 @@
+from os import path, environ
+
 # Constants
 TITLE = "Fuzed"
+CONFIG_PATH = path.join(environ["APPDATA"], TITLE, "config.cfg")
 
 AUTH_LEVELS = ["Super-user",
                "Administrator",
